d0ceb562a7b1582e363489e00eceb4ebe315a665
Max <massimo.odisio@gmail.com>
2026-06-11 12:06:00 +0200

    adds print headers on tcp listener

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -35,6 +35,12 @@ func main() {
 			fmt.Printf("- Method: %s\n", req.RequestLine.Method)
 			fmt.Printf("- Target: %s\n", req.RequestLine.RequestTarget)
 			fmt.Printf("- Version: %s\n", req.RequestLine.HttpVersion)
+			if len(req.Headers) != 0 {
+				fmt.Println("Headers:")
+				for k, v := range req.Headers {
+					fmt.Printf("- %s: %s\n", k, v)
+				}
+			}
 			fmt.Println("===========================")
 			fmt.Printf("Connection closed for %s\n", c.RemoteAddr())
 		}(conn)
